6102e47a94420228dcd1583c5117667e3b1b9ab2
vikash7001 <sgcapricorn.vikash@gmail.com>
2025-12-25 23:18:27 +0530

    server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 const app = require("./app");
 
-// 🔒 REQUIRED FOR RENDER
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`TallyInsight API running on port ${PORT}`);
 });
